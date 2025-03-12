b10cf103d38cdc750739e98ff6efafc57599f108
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-13 01:25:20 +0300

    Initial Commit

---
+++ BookFlight/BookFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookFlightApp.swift
+//  BookFlight
+//
+//  Created by MARK on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
